709ac8d1ea6ace35f285125eff7c199df97ccacc
Ashish <ashishkrgupta.7890@gmail.com>
2026-06-24 10:54:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/IntApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IntApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
